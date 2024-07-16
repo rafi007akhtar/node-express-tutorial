7deb918350d05796f1412721cae6d7793d1e3f79
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-17 03:12:46 +0530

    served app with HTML, CSS, JS, and image files using purely NodeJS

--- freeCodeCampCourses/express-tutorial/app.js
+++ freeCodeCampCourses/express-tutorial/app.js
@@ -1,4 +1,13 @@
 const http = require("http");
+const fs = require("fs");
+
+// const homepage = fs.readFileSync("./home.html");
+
+const NAVBAR_ROOT_PATH = "./navbar-app";
+const navbarHTML = fs.readFileSync(`${NAVBAR_ROOT_PATH}/index.html`);
+const navbarCSS = fs.readFileSync(`${NAVBAR_ROOT_PATH}/styles.css`);
+const navbarJS = fs.readFileSync(`${NAVBAR_ROOT_PATH}/browser-app.js`);
+const navbarLogo = fs.readFileSync(`${NAVBAR_ROOT_PATH}/logo.svg`);
 
 const server = http.createServer(
   // this will be called everytime this server is hit
@@ -6,10 +15,28 @@ const server = http.createServer(
     console.log("user hit the server");
     let statusCode = 200;
     let templateCode = "";
+    let contentType = "text/html";
 
     switch (request.url) {
       case "/":
-        templateCode = "<h1>This is the homepage</h1>";
+        // templateCode = "<h1>This is the homepage</h1>";
+        // templateCode = homepage;
+        templateCode = navbarHTML;
+        break;
+
+      case "/styles.css":
+        templateCode = navbarCSS;
+        contentType = "text/css";
+        break;
+
+      case "/browser-app.js":
+        templateCode = navbarJS;
+        contentType = "text/javascript";
+        break;
+
+      case "/logo.svg":
+        templateCode = navbarLogo;
+        contentType = "image/svg+xml";
         break;
 
       case "/about":
@@ -22,7 +49,7 @@ const server = http.createServer(
     }
 
     res.writeHead(statusCode, {
-      "content-type": "text/html", // so that the text sent gets formatted as HTML
+      "content-type": contentType, // so that the text sent gets formatted as HTML
       // "content-type": "text/plain", // here the text will be treated as pure text
     });
     res.write(templateCode);
