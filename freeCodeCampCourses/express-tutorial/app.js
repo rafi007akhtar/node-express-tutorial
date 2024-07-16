1d55e69716540ab67546f585cf166255565dbf1d
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-17 02:44:19 +0530

    added routes to the server, and segregated statusCode and templateCode based on the routes

--- freeCodeCampCourses/express-tutorial/app.js
+++ freeCodeCampCourses/express-tutorial/app.js
@@ -4,11 +4,29 @@ const server = http.createServer(
   // this will be called everytime this server is hit
   (request, res) => {
     console.log("user hit the server");
-    res.writeHead(200, {
+    let statusCode = 200;
+    let templateCode = "";
+
+    switch (request.url) {
+      case "/":
+        templateCode = "<h1>This is the homepage</h1>";
+        break;
+
+      case "/about":
+        templateCode = "This is the about page";
+        break;
+
+      default:
+        statusCode = 404;
+        templateCode = `Oops, page not found. \nPlease try a different URL.`;
+    }
+
+    res.writeHead(statusCode, {
       "content-type": "text/html", // so that the text sent gets formatted as HTML
       // "content-type": "text/plain", // here the text will be treated as pure text
     });
-    res.write("<h1>Home page</h1>");
+    res.write(templateCode);
+
     res.end();
   }
 );
