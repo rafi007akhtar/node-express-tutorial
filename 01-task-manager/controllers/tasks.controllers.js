dd2d9648ff2716f029ebc522487b0616b4f603c0
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-04 17:13:36 +0530

    set up task schema and used it to add new task

--- 01-task-manager/controllers/tasks.controllers.js
+++ 01-task-manager/controllers/tasks.controllers.js
@@ -1,9 +1,17 @@
+const Task = require("../models/tasks.model");
+
 function getAllTasks(request, res) {
   res.send("placeholder for all tasks.");
 }
 
-function createTask(request, res) {
-  res.json(request.body);
+async function createTask(request, res) {
+  try {
+    const task = await Task.create(request.body);
+    res.status(201).json(task);
+  } catch (e) {
+    console.log(e);
+    res.status(400).send(e.toString());
+  }
 }
 
 function getTask(request, res) {
