ee33c8bd0602b985e9f7019813777f65d090d48c
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-04 15:17:28 +0530

    connecting the app with DB using cred from dotenv file before spinning up the server

--- 01-task-manager/app.js
+++ 01-task-manager/app.js
@@ -1,20 +1,35 @@
 console.clear();
 
+const connectDB = require("./db/connect");
 const express = require("express");
 const app = express();
 const router = require("./routes/tasks.routes");
+
+require("dotenv").config();
 const port = process.env?.port || 3000;
 
 const BASE_PATH = `/api/v1`;
 const TASK_PATH = `${BASE_PATH}/tasks`;
 
 app.use(express.json());
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
-});
 
 app.get("/hello", (request, res) => {
   res.send("Task manager app");
 });
 
 app.use(TASK_PATH, router);
+
+async function start() {
+  console.log("Trying to connect to DB...");
+  const { err } = await connectDB(process.env.MONGO_URI);
+  if (err) {
+    console.error("Unable to connect to DB:", err);
+    return;
+  }
+
+  app.listen(port, () => {
+    console.log(`App running on port ${port}`);
+  });
+}
+
+start();
