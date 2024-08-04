ec58b71997bca352c45220893436a4789abd0434
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-04 21:17:57 +0530

    added validators to the schema

--- 01-task-manager/models/tasks.model.js
+++ 01-task-manager/models/tasks.model.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "cannot be empty"],
+    trim: true,
+    maxlength: [20, "cannot be longer than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 const Task = mongoose.model("Task", TaskSchema);
 
