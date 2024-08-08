b1388508642aaa04d34acb7510eb25ed252a4d8d
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-09 01:07:31 +0530

    implemented get all products and static routes with some querying

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,9 +1,26 @@
+const Product = require("../models/product");
+
 async function getAllProducts(request, res) {
-  res.status(200).json([1, 2, 3]);
+  const schemaKeys = Product.schema.obj;
+  const requestedKeys = request.query;
+  const searchKeys = {};
+  for (let rk in requestedKeys) {
+    if (rk in schemaKeys) {
+      searchKeys[rk] = requestedKeys[rk];
+    }
+  }
+  if ("name" in searchKeys) {
+    searchKeys.name = { $regex: searchKeys.name, $options: "i" };
+  }
+  const products = await Product.find(searchKeys);
+  res.status(200).json({ products, nbHits: products.length });
 }
 
 async function getAllProductsStatic(request, res) {
-  res.status(200).json({ msg: "static" });
+  const featuedProducts = await Product.find({
+    featured: true,
+  });
+  res.status(200).json({ featuedProducts, nbHits: featuedProducts.length });
 }
 
 module.exports = { getAllProducts, getAllProductsStatic };
