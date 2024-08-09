fa605a37ba5309c3734ac5784419dccf3ad3218d
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-10 00:30:34 +0530

    implemented numeric filters and passed them to the search query

--- 02-store-api/controllers/products.js
+++ 02-store-api/controllers/products.js
@@ -1,5 +1,13 @@
 const Product = require("../models/product");
 
+const operatorMap = {
+  ">=": "$gte",
+  "<=": "$lte",
+  ">": "$gt",
+  "<": "$lt",
+  "=": "$eq",
+};
+
 async function getAllProducts(request, res) {
   // make sure only the valid keys that are a part of the schema are passed on in the query
   const schemaKeys = Product.schema.obj;
@@ -14,14 +22,35 @@ async function getAllProducts(request, res) {
     searchKeys.name = { $regex: searchKeys.name, $options: "i" };
   }
 
+  let { sort, numericFilters } = requestedKeys;
+
   // apply the sort; if no sort is passed, sort by the created date
-  let { sort } = requestedKeys;
   sort = sort || "createdAt";
   if (sort.indexOf(",") !== -1) {
     sort = sort.replaceAll(",", " ");
   }
 
-  const products = await Product.find(searchKeys).sort(sort);
+  // get and parse the numeric filters, and append them to the search query
+  const parsedFilters = {};
+  const numericFiltersArr = numericFilters.split(",") || [];
+  for (let filter of numericFiltersArr) {
+    let filterOperator;
+    for (let operator in operatorMap) {
+      if (filter.includes(operator)) {
+        filterOperator = operator;
+        break;
+      }
+    }
+    const [lhs, rhs] = filter.split(filterOperator);
+    const mappedOperator = operatorMap[filterOperator];
+    const filterCondition = {};
+    filterCondition[mappedOperator] = +rhs;
+    parsedFilters[lhs] = filterCondition;
+  }
+
+  const products = await Product.find({ ...searchKeys, ...parsedFilters }).sort(
+    sort
+  );
   res.status(200).json({ products, nbHits: products.length });
 }
 
