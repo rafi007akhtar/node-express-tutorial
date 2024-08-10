bd669ec4afffdaf04a8cc97d25a5f3ef02e61321
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-10 19:54:39 +0530

    set up jwt authentication on the login and dashboard routes

--- 03-jwt-basics/controllers/main.js
+++ 03-jwt-basics/controllers/main.js
@@ -1,4 +1,5 @@
 const CustomAPIError = require("../errors/custom-error");
+const jwt = require("jsonwebtoken");
 
 async function login(request, res) {
   const { username, password } = request.body;
@@ -6,13 +7,35 @@ async function login(request, res) {
     throw new CustomAPIError("Username and / or password not provided", 400);
   }
 
-  res.send("Fake login / sign up / register route");
+  const id = new Date().toISOString(); // this should be unique for each user
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  }); // Do NOT send password here - it's a bad practise! Instead, send the unique key and a secret string
+
+  res.status(200).json({ token, message: "user created" });
 }
 
 async function dashboard(request, res) {
+  const authHeader = request.headers.authorization || "";
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("Token invalid or empty", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+  let decodedInfo;
+  try {
+    decodedInfo = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (e) {
+    return res.status(401).json({
+      msg: "Unable to verify token",
+      err: e,
+    });
+  }
+
+  const { username } = decodedInfo;
   const luckyNumber = Math.floor(Math.random() * 100);
   res.status(200).json({
-    msg: `Welcome, user`,
+    msg: `Welcome, ${username}`,
     secret: `Your secret lucky number is: ${luckyNumber}`,
   });
 }
