99c3c731621dda9b12d9f3cf6ac9d0f8911d4235
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-10 21:11:22 +0530

    refactored error handling for indiviual errors to be handled in their own files with a dedicated package

---
+++ 03-jwt-basics/errors/index.js
@@ -0,0 +1,5 @@
+const BadRequestError = require("./bad-request");
+const CustomAPIError = require("./custom-error");
+const UnauthenticatedError = require("./unauthenticated");
+
+module.exports = { BadRequestError, CustomAPIError, UnauthenticatedError };
