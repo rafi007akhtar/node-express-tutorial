f22270e9f6789ec984788bfa189915d610f5f574
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-18 22:45:30 +0530

    imported packages for security and used them as middleware

--- 04-jobs-api/app.js
+++ 04-jobs-api/app.js
@@ -6,15 +6,31 @@ const connectDB = require("./db/connect");
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 const authGuard = require("./middleware/authentication");
+
+// packages for security
+const helmet = require("helmet"); // adds a bunch of headers for security purposes
+const cors = require("cors"); // to configure which domains are allowed to access the server
+const xss = require("xss-clean"); // to sanitize req.params, req.query and req.body
+const rateLimit = require("express-rate-limit"); // to put limit for API requests
+
 const express = require("express");
 const app = express();
+app.set("trust proxy", 1); // for publishing the app to Heroku
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+// middleware for security
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+  })
+);
+app.use([helmet(), cors(), xss()]);
+
 app.use(express.json());
-// extra packages
 
 // routes
 app.use("/api/v1/auth", authRouter);
